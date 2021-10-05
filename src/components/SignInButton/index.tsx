f16677d68748624737b8b8ce623c8cacde93d112
Lucas da Mata <lucas.mata@b2list.com>
2021-10-05 10:13:27 -0300

    API Routes no Next.js Autenticação com Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,29 @@
-import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/client'
+
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggerIn = true;
-  return isUserLoggerIn ? (
+  const [session] = useSession(); 
+
+  console.log(session)
+  
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      lucasdamata
+      {session.user.name}
       <FiX  color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
